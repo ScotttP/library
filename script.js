f44c167e0aa328198e1932f27b78dbc918d9cd0b
Scott Pribe <Scottpribe@gmail.com>
2020-04-19 15:34:08 -0400

    input values from form will not show in the library

--- script.js
+++ script.js
@@ -1,46 +1,79 @@
 let myLibrary = [];
-
-let bookTitle = document.getElementById('bookTitle').value
-let bookAuthor = document.getElementById('bookAuthor').value
-let bookPages = document.getElementById('bookPages').value
-let readOrNot = document.getElementById('readOrNot').value
+let addTableRow;
+let bookTitle;
+let bookAuthor;
+let bookPages;
+let readOrNot;
 
 //QUERY SELECTORS
-let tableContents = document.querySelector('#tableContents');
-let addToBookListButton = document.querySelector('#addToBookListButton');
+const tableContents = document.querySelector('#tBody');
+const addToBookListButton = document.querySelector('#addToBookListButton');
+
 
 //EVENT LISTENERS
 addToBookListButton.addEventListener('click', () => {
-    render();
+    bookTitle = document.getElementById('bookTitle').value
+    bookAuthor = document.getElementById('bookAuthor').value
+    bookPages = document.getElementById('bookPages').value
+    readOrNot = document.getElementById('readOrNot').value
+    renderBooksInTable();
+
+})
+addToBookListButton.addEventListener('keypress', function(e) {
+    if (e.key == 'enter'){
+        renderBooksInTable();
+    }
 })
 
 const userInput = new Book (bookTitle,bookAuthor,bookPages,readOrNot)
 
-
+//CONSTRUCTORS
 function Book (title,author,pages,readOrNot) {
     this.title = title
     this.author = author
     this.pages = pages
     this.readOrNot = readOrNot
 }
-
+//PROTOTYPES
 Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this.title,this.author,this.pages,this.readOrNot)
     
 }
-
-function render () {
-    let addTableRow = document.createElement('tr')
-    tableContents.appendChild(addTableRow)
+//FUNCTIONS
+function renderBooksInTable () {
+    addTableRow = document.createElement('tr')
+    tBody.appendChild(addTableRow)
     for (let i = 0; i <= myLibrary.length; i++){
         let addTableRowContents = document.createElement('td')
         addTableRowContents.textContent = myLibrary[i]
         addTableRow.appendChild(addTableRowContents)
+    
     }
+    let tableRowIndex = addTableRow.rowIndex
+    addDeleteButton();
+    addDeleteListeners(tableRowIndex);
+}
+function addDeleteButton () {
+    let deleteButton = document.createElement('button')
+    deleteButton.classList.add('deleteButton')
+    deleteButton.textContent = 'DELETE'
+    addTableRow.appendChild(deleteButton)
     
 }
 
-//CALLS
-userInput.addBookToLibrary()
+function addDeleteListeners (tableRowIndex) {
+   let deleteButtonSelector =  document.querySelectorAll('.deleteButton');
+    deleteButtonSelector.forEach((deleteButton) => {
+        deleteButton.addEventListener('click', () => {
+           console.log('need to delete this current row')
+        })
+    });
+}
 
+
+
+//CALLS
+
+userInput.addBookToLibrary()
+console.log(bookTitle)
 
