5c020d446efd2e61965195dcf7f4ab9def3069f4
Scott Pribe <Scottpribe@gmail.com>
2020-04-18 21:42:02 -0400

    added render loop to JS file

---
+++ script.js
@@ -0,0 +1,41 @@
+let myLibrary = [];
+
+let bookTitle = document.getElementById('bookTitle').value
+let bookAuthor = document.getElementById('bookAuthor').value
+let bookPages = document.getElementById('bookPages').value
+let readOrNot = document.getElementById('readOrNot').value
+
+//QUERY SELECTORS
+let tableContents = document.querySelector('#tableContents');
+
+
+const userInput = new Book (bookTitle,bookAuthor,bookPages,readOrNot)
+
+
+function Book (title,author,pages,readOrNot) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.readOrNot = readOrNot
+}
+
+Book.prototype.addBookToLibrary = function () {
+    myLibrary.push(this.title,this.author,this.pages,this.readOrNot)
+    
+}
+
+function render () {
+    let addTableRow = document.createElement('tr')
+    tableContents.appendChild(addTableRow)
+    for (let i = 0; i <= myLibrary.length; i++){
+        let addTableRowContents = document.createElement('td')
+        addTableRowContents.textContent = myLibrary[i]
+        addTableRow.appendChild(addTableRowContents)
+    }
+    
+}
+
+//CALLS
+userInput.addBookToLibrary()
+render();
+
